47508c5763d2be55441a35b61c6ab972106ba406
Andrei Olteanu <a.olteanu198@yahoo.com>
2017-08-03 14:22:11 +0300

    First commit

---
+++ WhatsThat/WhatsThat/CameraVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WhatsThat
+//
+//  Created by Mac on 03/08/2017.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    
+    
+}
+
